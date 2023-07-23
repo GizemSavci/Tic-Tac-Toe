6574d6277830ade36718a86e8f613f1e5075583b
Gizem Savci <savci.gizem02@gmail.com>
2023-07-23 13:39:06 +0200

    Update Tic-Tac-Toe.py

--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -1,11 +1,21 @@
 print("Welcome to Tic Tac Toe Game!")
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+CurrentPlayer = "X"
+Winner = None
 
+#Board
 def printBoard(board):
     print(board[0] + " | " + board[1] + " | " + board[2])
     print("---------")
     print(board[3] + " | " + board[4] + " | " + board[5])
     print("---------")    
     print(board[0] + " | " + board[1] + " | " + board[2])
+#printBoard(board)
 
-printBoard(board)
+#Player Input
+def PlayerInput(board):
+    inp = int(input("Enter a number 1-9: "))
+    if inp >= 1 and inp <= 9 and board[inp-1] == " ":
+        board[inp-1] = CurrentPlayer
+    else:
+        print("Oops! Player is already in that spot.")
